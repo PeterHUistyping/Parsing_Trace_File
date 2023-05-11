501ced68c12fc8d80aa450bb91d11cd36ece1c1c
PeterHUistyping <2447276617@qq.com>
2023-05-11 21:36:16 +0100

    Update tools

--- parsing.java
+++ parsing.java
@@ -1,17 +1,67 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class parsing{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Set<String> api=new HashSet<>();
         try {
-          File myObj = new File("filename.txt");
+          String filename = "ty";
+          File myObj = new File(filename+".txt");
           Scanner myReader = new Scanner(myObj);
           while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            System.out.println(data);
+            // char[] API=new char[100];
+            char[] dataC= data.toCharArray();
+            int i=0;
+            for(char ch:dataC){
+                i++;
+                if(ch=='('){
+                  
+                    break;
+                }
+                // System.out.println(ch);
+            
+                // API[i++]=ch;
+            }
+            // API[i++]='\0';
+            // String apii=API.toString();
+            // System.out.println(data.substring(0,i-1));
+            api.add(data.substring(0,i-1));
           }
+          List<String> sortedList = new ArrayList<>(api);
+          Collections.sort(sortedList);
+          for (String apii:sortedList){
+            System.out.println(apii);
+          }
+          System.out.println("\n\n\n");
+          
+          System.out.println("Total API size:");
+
+          System.out.println(sortedList.size());
+
+          System.out.println("\n\n\n");
+
+          FileWriter fileWriter = new FileWriter(filename+"UniqueAPI.txt");
+          PrintWriter printWriter = new PrintWriter(fileWriter);
+          for (String apii:sortedList){
+            printWriter.print(apii+'\n');
+          }
+
+          printWriter.close();
           myReader.close();
+
+        
+
         } catch (FileNotFoundException e) {
           System.out.println("An error occurred.");
           e.printStackTrace();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Parsing Trace File
+Parsing Trace File  
+
+## Design Idea
+
+Extract the first non bracket from each line, assuming that each entry occupys only one line.
+Output data will be stored in a set to remove duplicate.
